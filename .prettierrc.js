fd6a819e52866361c1db259413a0d9e46c28de1e
MrOlmb <elvisolembe1@gmail.com>
2025-01-28 14:29:12 +0100

    fix:new .env file

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // Omit parentheses when an arrow function has only one parameter
+  arrowParens: 'avoid',
+  // Do not add spaces inside brackets
+  bracketSpacing: true,
+  // Use Unix-style line endings
+  endOfLine: 'lf',
+  // true: Put > on the last line instead of on a new line
+  jsxBracketSameLine: false,
+  // Line width
+  printWidth: 100,
+  // Wrapping method
+  proseWrap: 'preserve',
+  // Semicolons
+  semi: false,
+  // Use single quotes
+  singleQuote: true,
+  // Indentation
+  tabWidth: 2,
+  // Use tabs for indentation
+  useTabs: false,
+  // Trailing commas: Add commas to the last line of multi-line objects and arrays
+  trailingComma: 'es5',
+};
